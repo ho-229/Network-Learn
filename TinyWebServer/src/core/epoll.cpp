2e72e37559a33480ca171a1f634885842d9b075d
ho-229 <56023510+ho-229@users.noreply.github.com>
2022-04-17 14:32:40 +0800

    Update epoll.cpp

--- TinyWebServer/src/core/epoll.cpp
+++ TinyWebServer/src/core/epoll.cpp
@@ -20,9 +20,13 @@ Epoll::Epoll()
 
 Epoll::~Epoll()
 {
+    close(
 #if defined (OS_LINUX)
-    close(m_epoll);
+        m_epoll
+#elif defined (OS_UNIX)
+        m_kqueue
 #endif
+    );
 }
 
 void Epoll::insert(AbstractSocket * const socket, bool exclusive)
@@ -41,8 +45,7 @@ void Epoll::insert(AbstractSocket * const socket, bool exclusive)
     epoll_ctl(m_epoll, EPOLL_CTL_ADD, socket->descriptor(), &event);
 # else                      // Unix
     struct kevent event;
-    EV_SET(&event, socket->descriptor(), EVFILT_READ,
-           exclusive ? EV_ADD | EV_CLEAR : EV_ADD, 0, 0, socket);
+    EV_SET(&event, socket->descriptor(), EVFILT_READ, EV_ADD | EV_CLEAR, 0, 0, socket);
     kevent(m_kqueue, &event, 1, nullptr, 0, nullptr);
 # endif
     ++m_count;
